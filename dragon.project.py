31c24a3345b774a09ba2af689d143981a87140b3
dragon3091 <89909316+dragon3091@users.noreply.github.com>
2021-09-01 14:01:28 +0200

    Add files via upload

---
+++ dragon.project.py
@@ -0,0 +1,30 @@
+import os
+os.system ('clear')
+
+print ("hello its tool to termux metasploit")
+print ("        ") 
+print ("TO START TYPE ^GO^ :")
+print ("_________________") 
+omar=input ("===>>> : " )
+if omar=="GO" : 
+    print ("OK LETS GO")
+    
+else :
+    print ("ERORR  " *9999)
+   
+print ("we can download metasploit in your termux (Tybe meta))")
+print ("________________") 
+omar1=input ("Type meta : ")
+if omar1=="meta" : 
+    print ("""pkg update && pkg upgrade - y
+
+pkg insatll wget
+
+pkg install ruby
+
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+
+./metasploit.sh""")
+else :
+    print ("ERORR" *1000)
+print ("MR DRAGON, H101")
